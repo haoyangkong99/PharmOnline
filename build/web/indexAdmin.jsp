1083df26047a8c2e7d674de6dc7ed0000dcfad9a
Cheong Chien Li <clcheong00@gmail.com>
2022-01-29 01:41:29 +0800

    completed function to calculate completed transactions today

--- build/web/indexAdmin.jsp
+++ build/web/indexAdmin.jsp
@@ -1,3 +1,4 @@
+<%@page import="com.bean.Report"%>
 <%@page import="com.bean.User"%>
 <!DOCTYPE html>
 <html lang="en">
@@ -246,14 +247,14 @@
                 </div>
 
                 <div class="card-body">
-                  <h5 class="card-title">Sales <span>| Today</span></h5>
+                  <h5 class="card-title">Completed Transactions <span>| Today</span></h5>
 
                   <div class="d-flex align-items-center">
                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                       <i class="bi bi-cart"></i>
                     </div>
                     <div class="ps-3">
-                      <h6>145</h6>
+                        <h6><% Report report = new Report(); out.println(String.valueOf(report.getDailyTransactions())); %></h6>
                       <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
 
                     </div>
--- src/java/com/bean/Report.java
+++ src/java/com/bean/Report.java
@@ -5,6 +5,13 @@
 package com.bean;
 
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 /**
  *
@@ -13,12 +20,98 @@ import java.io.Serializable;
 public class Report implements Serializable {
     
     private double sales;
+    private int transactions;
     private double revenue;
     private double profit;
     private int customers;
     private int suppliers;
     private int products;
+
+    public int getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(int transactions) {
+        this.transactions = transactions;
+    }
     
+    public double getSales() {
+        return sales;
+    }
+
+    public void setSales(double sales) {
+        this.sales = sales;
+    }
+
+    public double getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(double revenue) {
+        this.revenue = revenue;
+    }
+
+    public double getProfit() {
+        return profit;
+    }
+
+    public void setProfit(double profit) {
+        this.profit = profit;
+    }
+
+    public int getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(int customers) {
+        this.customers = customers;
+    }
+
+    public int getSuppliers() {
+        return suppliers;
+    }
+
+    public void setSuppliers(int suppliers) {
+        this.suppliers = suppliers;
+    }
+
+    public int getProducts() {
+        return products;
+    }
+
+    public void setProducts(int products) {
+        this.products = products;
+    }
+    
+    
+    public int getDailyTransactions() throws ClassNotFoundException, SQLException{
+        
+        LocalDateTime now = LocalDateTime.now();
+        String datetime = Timestamp.valueOf(now).toString();
+        String date = datetime.substring(0, 10);
+        
+        String driver = "com.mysql.jdbc.Driver";
+        String dbName = "pharmonline";
+        String url = "jdbc:mysql://localhost/" + dbName + "?";
+        String userName = "root";
+        String pw = "";
+        String query = "SELECT * FROM `pharmonline`.`order` WHERE `status` = 'Completed' AND `orderDateTime` BETWEEN '"+date+" 00:00:00' AND '"+date+" 23:59:59'";
+        
+        Class.forName(driver);  //Load Driver
+        Connection con = DriverManager.getConnection(url, userName, pw);  // Set Connection
+        Statement st = con.createStatement();   // create query
+        ResultSet rs = st.executeQuery(query); // Execute query
+        
+        int count = 0;
+        
+        while(rs.next()){
+            count++;
+        }
+        
+        st.close();
+        con.close();  
+        return count;
+    }
     
     
 }
